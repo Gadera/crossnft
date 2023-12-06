fdd102c198aaa6a4b9030d26a6868b360fcde942
webagent-dev <webagent24@gmail.com>
2023-12-06 20:11:22 +0100

    finished cta section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Header, Main, Card, Desc, Showcase, Security, Work} from './component/index'
+import { Header, Main, Card, Desc, Showcase, Security, Work, Cta} from './component/index'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
      <Showcase />
      <Security />
      <Work />
+     <Cta />
     </div>
   )
 }
