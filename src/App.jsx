d13ab7bd1ee2afa964faf03ebf138e6b38c7ea3c
webagent-dev <webagent24@gmail.com>
2023-12-13 08:11:19 +0100

    fix overflow bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <div className='app_container'>
     <Header />
     <Routes>
      <Route path="/" element={<Home />} />
      <Route path='/launch_app/transaction' element={<Trans />}/>
     {/* <Footer /> */}
     </Routes>
-    </>
+    </div>
   )
 }
 
