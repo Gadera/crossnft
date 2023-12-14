3538d5bd9dbeadb76796879a35e5f9c91a9bceb9
Gadera <deraganobi@gmail.com>
2023-12-14 21:27:38 +0100

    mobile menu bar

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import './header.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
 import Bounce from 'react-reveal/Bounce';
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
-const Header = () => {
-    const address = useAddress();
-  return (
-    
-    <div className='header_container'>
-        <Bounce left>
-        <h2 className='header_text'><Link to="/"> CrossNFTL Lab</Link></h2>
-        <ul className='list_container'>
-            <li className='list'>
-                <button className='list_btn'><Link to="/" className='list_link'>Home</Link></button>
-            </li>
-            <li className='list'>
-                <button className='list_btn' ><Link to='/' className='list_link'>Contact</Link></button>
-            </li>
-            <li className='list'>
-                <button className='list_btn'><Link to='/' className='list_link'>Support</Link></button>
-            </li>
-            <li className='list'>
-                <button className='list_btn'><Link to='/profile' className='list_link'>Profile</Link></button>
-            </li>
-        </ul>
-        {/* <button className='header_btn'>
-        Connect Wallet
-        <img
-            src="/walle.png"
-            className="header_wallet"
-            alt="header_wallet"
-        />
-        
-        </button> */}
-        <ConnectWallet />
-        </Bounce>
-    </div>
-    
-  )
-}
 
-export default Header
+const Header = () => {
+    const [clicked, setClicked] = useState(false);
+  
+    const handleClick = () => {
+      setClicked(!clicked);
+    };
+  
+    return (
+      <div className='header_container'>
+        <Bounce left>
+          <h2 className='header_text'>
+            <Link to="/">CrossNFTL Lab</Link>
+          </h2>
+          <ul id='list_container' className={clicked ? "list_container active" : "list_container"}>             <li className='list'>
+              <button className='list_btn'><Link to="/" className='list_link'>Home</Link></button>
+            </li>
+            <li className='list'>
+              <button className='list_btn'><Link to='/' className='list_link'>Contact</Link></button>
+            </li>
+            <li className='list'>
+              <button className='list_btn'><Link to='/' className='list_link'>Support</Link></button>
+            </li>
+            <li className='list'>
+              <button className='list_btn'><Link to='/profile' className='list_link'>Profile</Link></button>
+            </li>
+          </ul>
+          <button className='header_btn'>
+            Connect Wallet
+            <img
+              src="/walle.png"
+              className="header_wallet"
+              alt="header_wallet"
+            />
+          </button>
+          <ConnectWallet />
+        </Bounce>
+        <div id='mobile' onClick={handleClick}>
+          <i id="bar" className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+      </div>
+    );
+  };
+  
+export default Header;
